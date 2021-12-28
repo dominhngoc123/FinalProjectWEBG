d9ced6ce067c87e825dfd49838f66e521486b676
NgocDM1993 <ngocatp.52@gmail.com>
2021-12-29 04:33:08 +0700

    Remove Conflict

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -19,14 +19,6 @@ class BookRepository extends ServiceEntityRepository
         parent::__construct($registry, Book::class);
     }
 
-    public function getBookById($bookId): ?Book
-    {
-        return $this->createQueryBuilder('book')
-            ->where('book.id = :val')
-            ->setParameter('val', $bookId)
-            ->getQuery()
-            ->getOneOrNullResult();
-    }
     // /**
     //  * @return Book[] Returns an array of Book objects
     //  */
