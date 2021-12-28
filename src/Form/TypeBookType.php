12e6b035af380027f9717fc2ac84f7d67e128faa
TrangBui2801 <snowfall2801@gmail.com>
2021-12-29 02:38:25 +0700

    update type controller & reponsitory

---
+++ src/Form/TypeBookType.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Form;
+
+use App\Entity\Type;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class Type2Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        
+            ->add('TypeName', TextType::class, [
+                'label' => 'Type name',
+                'required' => true,
+                'attr' =>
+                [
+                    'maxlength' => 50
+                ]
+            ])
+            ->add('TypeDescription', TextType::class, [
+                'label' => 'Type description',
+                'required' => true,
+                'attr' =>
+                [
+                        'maxlength' => 50
+                ]
+            ])
+            // ->add('CreateAt')
+            // ->add('CreateBy')
+            // ->add('UpdateAt')
+            // ->add('UpdateBy')
+            // ->add('books')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Type::class,
+        ]);
+    }
+}
