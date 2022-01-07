39f088b9ea0d717ef9466d0656ffe8df67ccc6a5
TrangBui2801 <snowfall2801@gmail.com>
2022-01-08 02:31:25 +0700

    update

--- migrations/Version20220107192627.php
+++ migrations/Version20220107192627.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220107103956 extends AbstractMigration
+final class Version20220107192627 extends AbstractMigration
 {
     public function getDescription(): string
     {
